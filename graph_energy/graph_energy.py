a887c4f5cef9a596a490bc07816e0de0bb35c91f
Andrew Wildman <apw4@uw.edu>
2018-05-03 15:20:40 -0700

    Added a graph_energy module and GraphEnergy class

---
+++ graph_energy/graph_energy.py
@@ -0,0 +1,14 @@
+import keras
+import pandas
+import pickle
+
+
+class GraphEnergy(object):
+    """Insert docstring here"""
+
+    def __init__(self):
+        pass
+
+    """
+    Add other methods here
+    """
