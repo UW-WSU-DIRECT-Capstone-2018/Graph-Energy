f72a866492beb9224f2d6856ebe0e55d160900a8
torinfs <torins@uw.edu>
2018-05-07 22:03:21 -0700

    Added parse function with preliminary functionality

--- graph_energy/graph_energy.py
+++ graph_energy/graph_energy.py
@@ -1,14 +1,52 @@
 import keras
-import pandas
 import pickle
+import configparser
+import pandas as pd
 
 
 class GraphEnergy(object):
-    """Insert docstring here"""
+    '''
+    The GraphEnergy object contains all of the program functionality
+    The following methods contain the modules available to the user.
+
+    parse_input():
+        Input: A ConfigFile in the following format:
+
+            --------------------------
+            [Data]
+            path = path/to/data.dat
+
+            [Meta]
+            param = some_value
+            --------------------------
+
+        Output:
+            -- pandas DataFrame of data.dat file
+            -- meta parameter dictionary
+
+    '''
 
     def __init__(self):
         pass
 
-    """
-    Add other methods here
-    """
+    def parse_input(self, inputfile):
+
+        # Create configParser object and read in .inp
+        config = configparser.ConfigParser()
+        config.read(inputfile)
+
+        # Read input file variables
+        path = config.get('Data', 'path')
+        temp = config.getint('Meta', 'temp')
+        meta_params = {'Temp': temp}
+
+        # Read .dat file into Pandas DataFrame
+        data = pd.read_csv(path, delim_whitespace=True)
+
+        return data, meta_params
+
+
+if __name__ == '__main__':
+
+    GE = GraphEnergy()
+    GE.parse_input('file.inp')
